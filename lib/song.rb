7ac672755a4cc4d23d5879133f028b00406491d0
Cody Dupuis <ctdupuis7@gmail.com>
2019-09-27 18:00:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,7 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
+ 
 
   def self.all
     @@all
@@ -10,4 +11,34 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    @@all << song
+    song
+  end
+  
+  def self.new_by_name(name)
+    song = self.new 
+    song.name = name
+    song
+  end
+  
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    @@all << song
+    song
+  end
+  
+  def self.find_by_name(name)
+    @@all.each.find{|track| track.name == name}
+  end
+  
+  def self.find_or_create_by_name(name)
+    if 
+      name
+    else
+      name.class.create_by_name(name)
+    end
+  end
 end
