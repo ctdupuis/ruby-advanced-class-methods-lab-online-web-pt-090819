d36f5996273c9ea6316a39962702526f8d6cf4c4
Cody Dupuis <ctdupuis7@gmail.com>
2019-09-27 18:02:30 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,11 +34,11 @@ class Song
     @@all.each.find{|track| track.name == name}
   end
   
-  def self.find_or_create_by_name(name)
-    if self.find_by_name(name)
-      name.name
+  def self.find_or_create_by_name(track)
+    if self.find_by_name(track)
+      track.name
     else
-      self.create_by_name(name)
+      self.create_by_name(track)
     end
   end
 end
